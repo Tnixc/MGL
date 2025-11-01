356250117cb3cc8a0579390531519f5c6e5ebe62
Tnixc <tnixxc@gmail.com>
2025-11-01 02:38:58 -0400

    simple

--- examples/elle/examples/resources/burning_ship.fs
+++ examples/elle/examples/resources/burning_ship.fs
@@ -4,6 +4,6 @@ layout(location = 0) in vec2 fragTexCoord;
 layout(location = 0) out vec4 fragColor;
 
 void main() {
-    // Simple test: just output red
-    fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    // Simple test: output blue to verify shader is working
+    fragColor = vec4(0.0, 0.0, 1.0, 1.0);
 }
